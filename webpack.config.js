356aaac456a1555c4a0023ceecb83278b0bc49fd
a-sabagh <a.sabagh72@gmail.com>
2023-01-21 05:34:45 +0330

    install and configure html-webpack-plugin and css-loader mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
 	mode: "development",
@@ -8,5 +10,20 @@ module.exports = {
 	output: {
 		filename: '[name].js',
 		path: path.resolve(__dirname,'public')
-	}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
+			},
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			title: 'Rock Paper Scissor'
+		}),
+		new MiniCssExtractPlugin(),
+	]
 }
