6552ae743a3beb1eaffdfcbb9403b3cd07bb28a0
a-sabagh <a.sabagh72@gmail.com>
2023-01-21 06:55:58 +0330

    add footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import header from './Components/Header/Header.js'
+import footer from './Components/Footer/Footer.js'
 
 export default class {
 
 	render(){
 		let headerComponent = header('Rock Scissor Paper')
+		let footerComponent = footer('All Right Reserved 2023')
 		return (`
 			${headerComponent}
 			Application
+			${footerComponent}
 		`)
 	}
 
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import footer from './Footer.css'
+
+export default (props) => {
+	return `
+		<footer>
+			<p>All Right Reserved 2023</p>
+		</footer>
+	`
+}
