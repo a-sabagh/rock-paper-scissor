ec04e360d13d51567bfa55c6353b5cf81cac3970
a-sabagh <a.sabagh72@gmail.com>
2023-01-21 07:04:46 +0330

    add panel component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import header from './Components/Header/Header.js'
 import footer from './Components/Footer/Footer.js'
+import panel from './Components/Panel/Panel.js'
 
 export default class {
 
 	render(){
 		let headerComponent = header('Rock Scissor Paper')
 		let footerComponent = footer('All Right Reserved 2023')
+		let panelComponent = panel()
 		return (`
 			${headerComponent}
-			Application
+			${panelComponent}
 			${footerComponent}
 		`)
 	}
---
+++ src/Components/Panel/Panel.js
@@ -0,0 +1,10 @@
+import style from './Panel.css'
+
+export default () => {
+	return `
+		<div class="wrapper">
+			<div class="panel"></div>
+			<button class="start" onclick="start">Start</button>
+		</div>
+	`
+}
