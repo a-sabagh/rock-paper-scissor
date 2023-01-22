19ff60db3964299e8eba55135322b89ed1d3f468
a-sabagh <a.sabagh72@gmail.com>
2023-01-22 08:28:45 +0330

    prepare items with images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import header from './Components/Header/Header.js'
 import footer from './Components/Footer/Footer.js'
 import panel from './Components/Panel/Panel.js'
+import rock from './images/rock.png'
+import paper from './images/paper.png'
+import scissor from './images/scissor.png'
 
 export default class {
 
@@ -13,12 +16,15 @@ export default class {
 				counter--
 				if(counter <= 0){
 					 clearInterval(interval)
+					panel.innerHTML = ''
 					 resolve('done')
 				}
 			},1000)	
 		}).then((result)=>{
-			let items = ['Paper','Scissor','Rock']
-			document.querySelector('.panel').innerHTML = items[Math.floor(Math.random()*items.length)]
+			let items = [rock,paper,scissor]
+			let img = document.createElement('img')
+			img.src = items[Math.floor(Math.random()*items.length)]
+			document.querySelector('.panel').append(img)
 		})
 	}
 
